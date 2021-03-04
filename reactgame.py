a6583921a99e81b95bd10ec06eb7fbb862462b17
Felipe Maion <felipe.maion@gmail.com>
2021-03-04 02:32:01 -0300

    React Game with PIL

--- reactgame.py
+++ reactgame.py
@@ -1,14 +1,17 @@
 import pyautogui
+from PIL import ImageGrab
 ### CODE TO TRY TO CLICK FAST IN THE SITE: 
 # https://humanbenchmark.com/tests/reactiontime
-RGB_TRIGGER = [27, 219, 113]
+RGB_TRIGGER = (27, 219, 113, 255)
 
 while True:
     pos = pyautogui.position()
     x, y = pos.x, pos.y
     # trigged = pyautogui.pixelMatchesColor(x, y, RGB_TRIGGER )
-    pix = pyautogui.pixel(x,y)
-    if [pix.red, pix.green, pix.blue] == RGB_TRIGGER: 
+    # pix = pyautogui.pixel(x,y)
+    pix = ImageGrab.grab().load()[x,y]
+    # print(pix)
+    if pix == RGB_TRIGGER: 
         pyautogui.mouseDown(x=x,y=y, button='left')
         pyautogui.mouseUp(x=x,y=y, button='left')
         trigged = False
