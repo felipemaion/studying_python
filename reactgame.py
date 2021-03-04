ebbc34cd41848a90fd47b788c4a688576377f991
Felipe Maion <felipe.maion@gmail.com>
2021-03-04 02:57:25 -0300

    React Game with PIL with box 1 pixel

--- reactgame.py
+++ reactgame.py
@@ -9,7 +9,9 @@ while True:
     x, y = pos.x, pos.y
     # trigged = pyautogui.pixelMatchesColor(x, y, RGB_TRIGGER )
     # pix = pyautogui.pixel(x,y)
-    pix = ImageGrab.grab().load()[x,y]
+    bbox=(x,y,x+1,y+1)
+    # print(f"{x}, {y}, { bbox }")
+    pix = ImageGrab.grab(bbox=bbox).load()[0,0]
     # print(pix)
     if pix == RGB_TRIGGER: 
         pyautogui.mouseDown(x=x,y=y, button='left')
