99754609461cae2ce0857af7357577e8c815d5a4
Felipe Maion <felipe.maion@gmail.com>
2022-04-14 03:30:07 -0300

    Class based help
    	modified:   AdrianaNaomi20220413.py

--- AdrianaNaomi20220413.py
+++ AdrianaNaomi20220413.py
@@ -17,11 +17,13 @@ class Pessoa:
 
     def nova(self):
         try:
-            self.nome = input('Informe o seu Nome :')
-            self.idade = int(input('Informe a sua Idade :'))
-            self.sexo = input('Informe o seu Sexo M/F :')[0]
-        except ValueError:
-            print("Entre com um número inteiro")
+            self.nome = input('Informe o seu Nome:\t')
+            self.idade = int(input('Informe a sua Idade:\t'))
+            self.sexo = input('Informe o seu Sexo M/F:\t')[0].upper()
+            if self.sexo not in 'MF':
+                raise ValueError
+        except:
+            print("\tERRO:\n\t\tEntre com um número inteiro para idade.\n\t\tM ou F para o sexo.")
             return self.nova()
         
     ## definindo as comparações: <, <=, ==, >=, >, respectivamente:
@@ -61,17 +63,26 @@ def main():
     while r == 'S':
         nova_pessoa = Pessoa()
         pessoas.append(nova_pessoa)
-        print(f"\n\tPessoa: {nova_pessoa} adicionada com sucesso!")
-        r = str (input ('Deseja Continuar?(Enter=S) [S/N] ')).upper() or 'S'
+        print(f"\n\tPessoa: {nova_pessoa} adicionada com sucesso!\n")
+        r = str (input ('\tDeseja Continuar?(Enter=S) [S/N] ')).upper() or 'S'
+    # INPUT: (descomente o código abaixo, e mude r para 'N')
     # pessoas.append(Pessoa("Felipe", 36, "M"))
     # pessoas.append(Pessoa("Mirella", 38, "F"))
     # pessoas.append(Pessoa("Livia", 8, "F"))
-    print('Grupo:', pessoas)
-    print ('A pessoa mais velha é :', max(pessoas))
-    print ('A pessoa mais nova é :' , min(pessoas))
-    print('A média de idade do grupo é :', sum(pessoas) / len(pessoas))
-    print('Quantos homens tem mais de 30 anos :', len([pessoa for pessoa in pessoas if pessoa.sexo == 'M' and pessoa.idade > 30]))
-    print('Quantas mulheres tem menos de 18 anos :', len([pessoa for pessoa in pessoas if pessoa.sexo == 'F' and pessoa.idade < 18]))
+      
+    # OUTPUT:
+    # Grupo: ['Felipe, M, 36 anos', 'Mirella, F, 38 anos', 'Livia, F, 8 anos']
+    # A pessoa mais velha é : Mirella, F, 38 anos
+    # A pessoa mais nova é : Livia, F, 8 anos
+    # A média de idade do grupo é : 27.333333333333332
+    # Quantos homens tem mais de 30 anos : 1
+    # Quantas mulheres tem menos de 18 anos : 1
+    print('\nGrupo:', *pessoas, sep="\n\t")
+    print ('\nA pessoa mais velha é :\t', max(pessoas))
+    print ('A pessoa mais nova é :\t' , min(pessoas))
+    print(f"A média de idade do grupo é :\t { sum(pessoas) / len(pessoas) } anos")
+    print('Quantos homens tem mais de 30 anos :\t', len([pessoa for pessoa in pessoas if pessoa.sexo == 'M' and pessoa.idade > 30]))
+    print('Quantas mulheres tem menos de 18 anos :\t', len([pessoa for pessoa in pessoas if pessoa.sexo == 'F' and pessoa.idade < 18]))
     
 if __name__ == '__main__':
     main()
